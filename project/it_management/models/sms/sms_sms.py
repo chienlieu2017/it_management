46761bab66714a7c008738d451f9a1137870cd18
Thanh Thuan Lieu <ubizgit@gmail.com>
2017-04-01 21:29:54 +0700

    [ADD] Add function send SMS and refactoring source

--- project/it_management/models/sms/sms_sms.py
+++ project/it_management/models/sms/sms_sms.py
@@ -6,13 +6,27 @@
 ##############################################################################
 
 from odoo import api, fields, models
+import requests
 
 SMS_STATES = [('draft', 'Draft'),
               ('100', 'Sent'),
-              ('104', 'Brand name is not exits'),
+              ('103', 'Empty Money'),
+              ('104', 'No Brand Name'),
               ('118', 'Message Invalid'),
-              ('119', 'Brand name at least 20 number phones'),
-              ('131', 'Brand name over 422 characters')]
+              ('119', 'Need more than 20'),
+              ('131', 'Brand name too long')]
+
+SmsXmlData = """<RQST>
+                <APIKEY>%s</APIKEY>
+                <SECRETKEY>%s</SECRETKEY>
+                <NAME>HMS Esms</NAME>
+                <CONTENT>%s</CONTENT>
+                <CONTACTS>
+                    <CUSTOMER>
+                        <PHONE>%s</PHONE>
+                    </CUSTOMER>"
+                </CONTACTS>
+            </RQST>"""
 
 
 class SmsSms(models.Model):
@@ -20,7 +34,8 @@ class SmsSms(models.Model):
     _description = "SMS"
 
     name = fields.Char('Name', readonly=True)
-    mobile_phone = fields.Integer('Mobile Phone', required=True)
+    sms_id = fields.Char('SMSID', readonly=True)
+    mobile_phone = fields.Char('Mobile Phone', required=True)
     message = fields.Text('Message Content', required=True)
     datetime_sent = fields.Datetime('Datetime Sent', readonly=True)
     state = fields.Selection(SMS_STATES, 'Status', default='draft')
@@ -36,3 +51,25 @@ class SmsSms(models.Model):
         next_sequence = self.env['ir.sequence'].next_by_code('sms.sms')
         vals.update({'name': next_sequence})
         return super(SmsSms, self).create(vals)
+
+    @api.multi
+    def button_send(self):
+        ApiKey = self.env['ir.config_parameter'].get_param('sms_api_key')
+        SecretKey = self.env['ir.config_parameter'].get_param('sms_secret_key')
+        SmsUrl = self.env['ir.config_parameter'].get_param('sms_url')
+        for sms in self:
+            message = sms.sms_template_id and sms.sms_template_id.content + \
+                " " + sms.message or sms.message
+            sms_data = SmsXmlData % (ApiKey, SecretKey, message,
+                                     sms.mobile_phone)
+            r = requests.post(SmsUrl, data=sms_data)
+            str_result = r.text.encode('utf-8').replace('</', '<')
+            list_result = str_result.split('<CodeResult>')
+            code_result = False
+            error_message = ''
+            if list_result >= 3:
+                code_result = list_result[1]
+                if 'ErrorMessage' in list_result[2]:
+                    error_message = list_result[2].split('<ErrorMessage>')[1]
+            sms.write({'state': code_result and str(code_result) or 'draft',
+                       'error_message': error_message})
