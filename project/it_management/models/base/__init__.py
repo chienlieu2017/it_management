175cec13e9131961d1b0e5212c6818a687c3949c
Chien Nguyen Minh <chien@trobz.com>
2017-05-13 16:12:08 +0700

    [ADD] Security admin profile

--- project/it_management/models/base/__init__.py
+++ project/it_management/models/base/__init__.py
@@ -4,6 +4,7 @@
 #    Copyright 2009-2017 4Leaf Team
 #
 ##############################################################################
+import ir_ui_menu
 import monthly_report
 import res_partner
 import res_partner_department
